b9f86853b9f4dfbdedde2068dfbf91a95fe09db5
kickthatnga <kickthatniggabitchouttheplane@atomicmail.io>
2026-04-27 02:55:24 +0530

    Update ratelimit.js

--- ratelimit.js
+++ ratelimit.js
@@ -33,7 +33,7 @@ async function getCurrentIP() {
 async function sendSignup(count) {
     const rand = randomString(8);
     const username = `stress_${rand}`;
-    const email = `${username}@duck.com`;
+    const email =    `thrower-silly-tall@duck.com`;//`${username}@duck.com`;
     const password = `Pass${rand}123!`;
 
     const payload = { username, email, password };
